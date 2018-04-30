8c3c81e4efb07ff34fe846f911044f937abdc650
András <k.andras9642@gmail.com>
2018-04-30 19:06:09 +0200

    Penis

--- src/main/java/projlab/Main.java
+++ src/main/java/projlab/Main.java
@@ -1,6 +1,7 @@
 package projlab;
 
 public class Main {
+    //TODO: Make Penis Map
     public static void main(String[] args) {
         MainController mainController = new MainController();
         mainController.initializeComponents();
